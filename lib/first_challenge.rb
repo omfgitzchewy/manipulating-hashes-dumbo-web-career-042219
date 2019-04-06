44b7fb5fe1ef8140e72fc6a95d21232b5ea583b6
Aaron Chu <aaronchu357@gmail.com>
2019-04-06 20:38:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  return contacts["Freddy Mercury"].delete_if {|k,v| v == "strawberry"}
+  contacts["Freddy Mercury"].delete_if {|k,v| v == "strawberry"}
 
   #remember to return your newly altered contacts hash!
 
