92ddb006dc607ed6797837d0e0964451824130f1
Aaron Chu <aaronchu357@gmail.com>
2019-04-06 20:49:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"].each do |k, v|
-    k.delete_if {|v| v == "strawberry"}
+    :favorite_icecream_flavors.delete_if {|v| v == "strawberry"}
   end
   #remember to return your newly altered contacts hash!
   contacts
