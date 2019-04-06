e08d585adfb2c7d98276e429dd9880f89c01f41d
Aaron Chu <aaronchu357@gmail.com>
2019-04-06 20:26:57 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +14,15 @@ def first_challenge
   }
 
   #your code here
+  contacts.each do |name, data|
+    if name == "Freddy Mercury"
+      data.each do |k, v|
+        v.delete_if v == "strawberry"
+      end
+    end
 
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
